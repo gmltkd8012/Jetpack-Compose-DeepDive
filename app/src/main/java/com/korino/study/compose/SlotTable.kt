a02fabfb73a3ac255c55bbf1e1189227f5b72cda
LeeHeesang <gmltkd8012@naver.com>
2026-02-04 20:40:59 +0900

    [Korino] Compose Runtime 슬롯 테이블 동작 방식 2

--- app/src/main/java/com/korino/study/compose/SlotTable.kt
+++ app/src/main/java/com/korino/study/compose/SlotTable.kt
@@ -1,14 +1,23 @@
 package com.korino.study.compose
 
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.NonRestartableComposable
+//
+//@Composable
+//@NonRestartableComposable
+//fun ConditionalText() {
+//    if (a) {
+//        Text(a)
+//    } else {
+//        Text(b)
+//    }
+//}
+
 
 @Composable
-@NonRestartableComposable
-fun ConditionalText() {
-    if (a) {
-        Text(a)
-    } else {
-        Text(b)
-    }
+fun SlotScreen() {
+    Text(text = "text")
+    Button(onClick = {}) { }
 }
